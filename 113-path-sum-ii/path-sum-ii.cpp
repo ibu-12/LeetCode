8f215eb44957bfdee301c3b884f0e9dabafbe454
ibu-12 <125265659+ibu-12@users.noreply.github.com>
2025-02-04 11:33:59 +0530

    Time: 15 ms (26.75%) | Memory: 40.3 MB (19.18%) - LeetSync

--- 113-path-sum-ii/path-sum-ii.cpp
+++ 113-path-sum-ii/path-sum-ii.cpp
@@ -11,27 +11,23 @@
  */
 class Solution {
 public:
-    vector<vector<int>>ans;
-    vector<vector<int>> pathSumSol(TreeNode* root, int targetSum,vector<int>curr) {
-       
-        if(!root)
-            return ans;
-        if(!root->left && !root->right)
-        {
-            if(root->val==targetSum){
-                curr.push_back(root->val);
-                ans.push_back(curr);
-            }
-        }
-        curr.push_back(root->val);
-        int newSum=(targetSum)-root->val;
-        pathSumSol(root->left,newSum,curr);
-        pathSumSol(root->right,newSum,curr);
-        return ans;
+    std::vector<std::vector<int>> pathSum(TreeNode* root, int targetSum) {
+        std::vector<std::vector<int>> result;
+        backtrack(root, targetSum, {}, result);
+        return result;
     }
-    vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
-        vector<int>curr;
-        return pathSumSol(root,targetSum,curr);
 
-     }
+    void backtrack(TreeNode* root, int targetSum, std::vector<int> currentlist, std::vector<std::vector<int>>& result) {
+        if (root == nullptr) {
+            return;
+        }
+        currentlist.push_back(root->val);
+        if (root->left == nullptr && root->right == nullptr && root->val == targetSum) {
+            result.push_back(currentlist);
+        } else {
+            backtrack(root->left, targetSum - root->val, currentlist, result);
+            backtrack(root->right, targetSum - root->val, currentlist, result);
+        }
+        currentlist.pop_back();
+    }
 };
