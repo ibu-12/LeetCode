218cb37c0e11a7f7263b4a134196fe3a3eefe505
ibu-12 <125265659+ibu-12@users.noreply.github.com>
2025-02-06 09:48:26 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (45.62%) - LeetSync

--- 326-power-of-three/power-of-three.cpp
+++ 326-power-of-three/power-of-three.cpp
@@ -1,13 +1,10 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        if(n==0)return false;
-        
-        while(n>0){
-            if(n==1)return true;
-            if(n %3 !=0)break;
-            n /=3;
-        }
+        if(n==1)
+        return true;
+        if(n==0 || n%3!=0)
         return false;
+        return isPowerOfThree(n/3);
     }
 };
