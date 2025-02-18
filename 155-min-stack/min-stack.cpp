df908e97509d4e895add80062142b9b4ed0f8ce1
ibu-12 <125265659+ibu-12@users.noreply.github.com>
2025-02-18 10:48:51 +0530

    Time: 0 ms (100.00%) | Memory: 23.2 MB (72.07%) - LeetSync

---
+++ 155-min-stack/min-stack.cpp
@@ -0,0 +1,29 @@
+class MinStack {
+public:
+
+    vector<pair<int, int>> arr;
+
+    MinStack() {}
+    
+    void push(int val) {
+        if (arr.empty()) {
+            arr.push_back({val, val});
+        }
+        else {
+            arr.push_back({val, min(arr.back().second, val)});
+        }
+    }
+    
+    void pop() {
+        arr.pop_back();
+    }
+    
+    int top() {
+        return arr.back().first;
+    }
+    
+    int getMin() {
+        return arr.back().second;
+    }
+};
+
