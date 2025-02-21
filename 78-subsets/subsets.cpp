ddac21b9e81448a5ffc8482130d0b0c78c6415aa
ibu-12 <125265659+ibu-12@users.noreply.github.com>
2025-02-21 09:35:10 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (81.64%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -2,18 +2,20 @@ class Solution {
 public:
 
     void f(int ind ,vector<int>& nums,vector<vector<int>>& ans,int n,vector<int>&ds){
-        
-        ans.push_back(ds);
-        for(int i=ind;i<n;i++){
-            ds.push_back(nums[i]);
-            f(i+1,nums,ans,n,ds);
-            ds.pop_back();
+        if(ind==nums.size()){
+            ans.push_back(ds);
+            return;
         }
+        ds.push_back(nums[ind]);
+        f(ind+1,nums,ans,n,ds);
+        ds.pop_back();
+        f(ind+1,nums,ans,n,ds);
     }
 
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> ans;
         int n=nums.size();
+        // ans.push_back(vector<int>());
         vector<int>ds;
         f(0,nums,ans,n,ds);
         return ans;
