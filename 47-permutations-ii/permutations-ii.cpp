0566e34b40fda56503482c5a6ac8f86085a234a1
ibu-12 <125265659+ibu-12@users.noreply.github.com>
2025-05-26 20:59:08 +0530

    Time: 4 ms (51.62%) | Memory: 12.5 MB (47.78%) - LeetSync

--- 47-permutations-ii/permutations-ii.cpp
+++ 47-permutations-ii/permutations-ii.cpp
@@ -1,33 +1,15 @@
 class Solution {
-
-private:
-
-    bool f(vector<int>& nums, int start, int end) {
-        for (int i = start; i < end; i++) {
-            if (nums[i] == nums[end]) return true;
-        }
-        return false;
-    }
-
-    void f(int ind,vector<int>& nums,vector<vector<int>>&ans){
-
-        if(ind==nums.size()){
-            ans.push_back(nums);
-            return;
-        }
-        for(int i=ind;i<nums.size();i++){
-            if(f(nums,ind,i))continue;
-            swap(nums[ind],nums[i]);
-            f(ind+1,nums,ans);
-            swap(nums[ind],nums[i]);
-        }
-    }
-
 public:
     vector<vector<int>> permuteUnique(vector<int>& nums) {
         vector<vector<int>>ans;
-        // sort(nums.begin(),nums.end());
-        f(0,nums,ans);
+        set<vector<int>>st;
+        sort(nums.begin(),nums.end());
+        do{
+            st.insert(nums);
+        }while(next_permutation(nums.begin(),nums.end()));
+        for(auto  it:st){
+            ans.push_back(it);
+        }
         return ans;
     }
 };
